122fa5d42ee7430a692631b0098bcd7a9ea81655
Austin Perez <a.m.perez@wustl.edu>
2025-10-24 22:08:14 -0500

    Refactor database setup and migration scripts; remove init-db and seed-db scripts, update package.json for Prisma integration, and adjust pnpm commands for database operations.

---
+++ apps/api/prisma/migrations/20251025030301_/migration.sql
@@ -0,0 +1,23 @@
+-- DropIndex
+DROP INDEX "Appointment_vendorId_idx";
+
+-- DropIndex
+DROP INDEX "Appointment_ticketId_idx";
+
+-- DropIndex
+DROP INDEX "AuditLog_ticketId_idx";
+
+-- DropIndex
+DROP INDEX "Property_landlordId_idx";
+
+-- DropIndex
+DROP INDEX "Tenant_propertyId_idx";
+
+-- DropIndex
+DROP INDEX "Ticket_status_idx";
+
+-- DropIndex
+DROP INDEX "Ticket_propertyId_idx";
+
+-- DropIndex
+DROP INDEX "Ticket_tenantId_idx";
